70e3f01532c01b40d6539215805faa554ef063e2
ishimwe amiri <elamirizidani@gmail.com>
2025-07-20 16:06:44 +0200

    added reminder 12

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-  chunkSizeWarningLimit: 1000,
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        react: ['react', 'react-dom']
-      }
-    }
-  }
-}
+  plugins: [
+    react()
+  ],
+  base: '/',
 })
